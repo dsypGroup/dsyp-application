05ba86307d6bdb3b82204560558823e86098e959
didoChulz <subodha555@gmail.com>
2018-04-01 01:18:20 -0700

    Added comments and code formatting

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,70 +1,63 @@
 import Ember from 'ember';
 import utils from '../utils/utils';
 
-
 export default Ember.Controller.extend({
-  isLoginClicked: false,
-  isSignUpClicked: false,
-  isAuthenticated: false,
-  isHomeViewEnabled: false,
-  model: {},
+    isLoginClicked: false,
+    isSignUpClicked: false,
+    isAuthenticated: false,
+    isHomeViewEnabled: false,
+    model: {},
 
-  actions: {
-    clickLoginWinBtn: function() {
-      this.set('isLoginClicked', true);
-      this.set('isSignUpClicked', false);
-    },
+    actions: {
+        clickLoginWinBtn: function () {
+            this.set('isLoginClicked', true);
+            this.set('isSignUpClicked', false);
+        },
 
-    clickSignUpWinBtn: function() {
-      this.set('isLoginClicked', false);
-      this.set('isSignUpClicked', true);
-    },
+        clickSignUpWinBtn: function () {
+            this.set('isLoginClicked', false);
+            this.set('isSignUpClicked', true);
+        },
 
-    clickLoginBtn: function(user) {
-      var loginMsgElem = Ember.$('div#loginMsg');
-      var username = user.uname;
-      var password = user.pwd;
-      var that = this;
+        clickLoginBtn: function (user) {
+            var loginMsgElem = Ember.$('div#loginMsg');
+            var username = user.uname;
+            var password = user.pwd;
+            var that = this;
 
-      if (utils.validators.isAvailable(username) && utils.validators.isAvailable(password)) {
-        $.ajax({
-          type: "POST",
-          //url: "http://siteurl/api/authentication/login/&username=" + user.username + "&password=" + user.password,
-          url: "http://ec2-18-217-238-61.us-east-2.compute.amazonaws.com:3000/api/login",
-          data: { username: username, password: password}
-        }).then(function(resp){
-          console.log(resp);
-          // handle your server response here
+            //send a ajax request to the server to authenticate
+            if (utils.validators.isAvailable(username) && utils.validators.isAvailable(password)) {
+                $.ajax({
+                    type: "POST",
+                    url: "http://ec2-18-217-238-61.us-east-2.compute.amazonaws.com:3000/api/login",
+                    data: {username: username, password: password}
+                }).then(function (resp) {
+                    if (resp == 200) {
+                        that.set('isAuthenticated', true);
+                    } else if (resp == 0) {
+                        //Show password or username incorrect message
+                        loginMsgElem.css('background-color', '#e15848');
+                        loginMsgElem.html("Username Or Password is Incorrect").show();
+                        that.set('isAuthenticated', false);
+                    } else {
+                        //Show password or username empty message
+                        loginMsgElem.css('background-color', '#e15848');
+                        loginMsgElem.html("Authentication Failed").show();
+                    }
+                }).catch(function (error) {
+                    // handle errors here
+                });
+            } else {
+                //Show password or username empty message
+                loginMsgElem.css('background-color', '#e15848');
+                loginMsgElem.html("Username Or Password is empty").show();
+                that.set('isAuthenticated', false);
+            }
+        },
 
-          if (resp == 200) {
-            that.set('isAuthenticated', true);
-          } else if (resp == 0) {
-            //Show password or username incorrect message
-            loginMsgElem.css('background-color', '#e15848');
-            loginMsgElem.html("Username Or Password is Incorrect").show();
-            //loginMsgElem.html(loginMsg).show(); // Show request error message
-            that.set('isAuthenticated', false);
-          } else {
-			  //Show password or username empty message
-        loginMsgElem.css('background-color', '#e15848');
-        loginMsgElem.html("Authentication Failed").show();
-        //loginMsgElem.html(loginMsg).show(); // Show request error message
-		  }
-        }).catch(function(error){
-          // handle errors here
-        });
-      } else {
-        //Show password or username empty message
-        loginMsgElem.css('background-color', '#e15848');
-        loginMsgElem.html("Username Or Password is empty").show();
-        //loginMsgElem.html(loginMsg).show(); // Show request error message
-        that.set('isAuthenticated', false);
-      }
-    },
-
-    logOutBtnActionPerformed: function() {
-      this.set('isAuthenticated', false);
-      this.set('isHomeViewEnabled', false);
+        logOutBtnActionPerformed: function () {
+            this.set('isAuthenticated', false);
+            this.set('isHomeViewEnabled', false);
+        }
     }
-  }
 });
