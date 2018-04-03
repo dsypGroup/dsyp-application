a92ed0ad1d3c6343d30740c42a60ba851c8123fc
l_pamuditha <l.pamuditha@directfn.com>
2018-04-03 23:54:22 +0530

    account-settings.js

--- app/controllers/account-settings.js
+++ app/controllers/account-settings.js
@@ -6,6 +6,22 @@ export default Ember.Controller.extend({
       $(".page-wrapper").toggleClass('sidebar-opened');
       $(".page-wrapper").toggleClass('sidebar-closed');
     }
+
+    //onSaveClicked: function (users) {
+    //  var staticId = Math.floor(Date.now() / 1000);
+    //
+    //  var newRecord = this.store.createRecord('users', {
+    //    userName: users.userName,
+    //    currentPassword: users.password,
+    //    newPassword: users.newPassword,
+    //    confirmNewPassword: users.confirmNewPassword,
+    //    id: staticId
+    //  });
+    //
+    //  newRecord.save();
+
+      // this.set('isShowAddDevices', false);
+    //}
   }
 });
 
