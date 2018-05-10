4725ac9f99b90f9166f9dd284fe4272e557ef646
l_pamuditha <l.pamuditha@directfn.com>
2018-05-10 09:51:06 +0530

    Not navigating to same page issue fixed

--- app/components/side-bar.js
+++ app/components/side-bar.js
@@ -1,6 +1,8 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  isSideBarOpened: false,
+
   actions: {
     onToggleBtnClick: function () {
       $(".page-wrapper").toggleClass('sidebar-opened');
