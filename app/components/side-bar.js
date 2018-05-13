a96f2c8a49bc86acf498de367da0cd362f0d713f
yasadara <yasadaradiluni@gmail.com>
2018-05-13 15:49:48 -0500

    changed selected tab ui

--- app/components/side-bar.js
+++ app/components/side-bar.js
@@ -1,7 +1,17 @@
 import Component from '@ember/component';
+import Ember from 'ember';
 
 export default Component.extend({
   isSideBarOpened: false,
+
+  didRender() {
+    this._super(...arguments);
+    $('a').each(function() {
+      if ($(this).prop('href') == window.location.href) {
+        $(this).addClass('current');
+      }
+    });
+  },
 
   actions: {
     onToggleBtnClick: function () {
